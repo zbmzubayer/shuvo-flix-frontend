8b937c0e68e5a50106b51de012b219451399d522
ZBM Zubayer <zbmzubayer@gmail.com>
2025-10-10 23:18:23 +0600

    refactor: loading UI

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,10 @@
+import { Spinner } from '@/components/ui/spinner';
+
 export default function LoadingPage() {
-  return <div>LoadingPage</div>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <Spinner />
+      <p className="text-muted-foreground">Loading...</p>
+    </div>
+  );
 }
